feace45c58c6cf9d73540561915117c26d1fea91
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2018-01-12 14:39:59 +0000

    Added compilation script.

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# flags that need to be present
+cflags="-std=c++11 -fopenmp"
+
+# Extra flags: choose one of the two below
+# optional (optimization) flags
+flags="-Wall -Werror -O3 -ftree-vectorize -funroll-loops -ffast-math"
+# optional (debug) flags
+#flags="-Wall -Werror -g -fsanitize=address -fno-omit-frame-pointer"
+
+for f in testDensitySubGrid.cpp CommandLineOption.cpp CommandLineParser.cpp
+do
+  mpic++ $flags $cflags -c $f
+done
+
+mpic++ $flags $cflags -o testDensitySubGrid \
+       testDensitySubGrid.o CommandLineOption.o CommandLineParser.o
