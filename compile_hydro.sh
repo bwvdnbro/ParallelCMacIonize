82dff5b11460c8538b3e54c9fcd381e1c1b99c35
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2018-06-30 22:03:40 +0100

    Added hydro test compilation script to repository.

---
+++ compile_hydro.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+debug=
+
+while [ "$1" != "" ]
+do
+  case $1 in
+    -d | --debug ) debug=1;;
+  esac
+  shift
+done
+
+# flags that need to be present
+cflags="-std=c++11 -fopenmp -march=native"
+
+# Extra flags: choose one of the two below
+flags="-Wall -Werror -Wignored-qualifiers"
+if [ "$debug" = "1" ]
+then
+  # optional (debug) flags
+  flags="$flags -g -fsanitize=address -fno-omit-frame-pointer"
+  echo "Compiling with debug flags"
+else
+  # default (optimization) flags
+  flags="$flags -O3 -ftree-vectorize -funroll-loops -ffast-math"
+fi
+
+# library dependencies
+libs="-lmetis"
+
+for f in testHydro.cpp
+do
+  mpic++ $flags $cflags -c $f
+done
+
+mpic++ $flags $cflags -o testHydro testHydro.o $libs
